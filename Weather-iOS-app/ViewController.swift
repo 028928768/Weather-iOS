0502ee55f38f497ba4efadb31ca2ac3a796dd5fa
Simon Siwell <kantachat_2539@hotmail.com>
2022-01-01 20:59:50 +0700

    Initial Commit

---
+++ Weather-iOS-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-iOS-app
+//
+//  Created by Simon on 1/1/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
